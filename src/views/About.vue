328d65c75bebbbf8cc02f286d5e04302f2915114
iezlli <2922520570@qq.com>
2020-07-02 23:31:34 +0800

    about.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    sdf
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+.about {
+  &::before {
+    content: '';
+  }
+}
+</style>
